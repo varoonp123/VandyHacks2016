07b01ccbd00a694d1f4a799d8be873199042ce9f
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-11-12 16:14:33 -0600

    Fake schema

--- client/lib/main.js
+++ client/lib/main.js
@@ -1,45 +1,104 @@
-var ROOM_DATA = {
-	floors: [{
-		name: 'Floor 1',
-		rooms: [{
+var BUILDING_DATA = {
+	floors: {
+		'floor1': {
+			name: 'Floor 1'
+		},
+		'floor2':{
+			name: 'Floor 2'
+		}
+	},
+	rooms: {
+		'room1': {
 			name: 'Room 1',
-			spots: [{
-				open: true,
-				sensor: 'sensor1'
-			}, {
-				open: false,
-				sensor: 'sensor2'
-			}]
-		}, {
-			name: 'Room 2',
-			spots: [{
-				open: false,
-				sensor: 'sensor3'
-			}, {
-				open: false,
-				sensor: 'sensor4'
-			}, {
-				open: false,
-				sensor: 'sensor5'
-			}]
-		}]
-	}, {
-		name: 'Floor 2',
-		rooms: [{
-			name: 'Conference Room',
-			spots: [{
-				open: false,
-				sensor: 'sensor1'
-			}, {
-				open: false,
-				sensor: 'sensor2'
-			}]
-		}]
-	}]
+			floor: 'floor1'
+		},
+		'roomj': {
+			name: 'Meeting Room',
+			floor: 'floor2'
+		}
+	}
 }
 
-function getTag(tag, content){
-	return '<' + tag + '>' + content + '</' + tag + '>';
+var SENSOR_DATA = {
+	'sensor1': {
+		open: true,
+		room: 'room1'
+	},
+	'sensor2': {
+		open: false,
+		room: 'room1'
+	},
+	'sensor3': {
+		open: false,
+		room: 'roomj'
+	},
+	'sensor4': {
+		open: true,
+		room: 'room1'
+	}
+}
+
+function getTag(tag, content, opt){
+	var classList = false;
+	if(opt){
+		if(opt.classes){
+			classList = ' class="' + opt.classes.join(' ') + '"';
+		}
+	}
+	return '<' + tag + (classList ? classList : '') + '>' + content + '</' + tag + '>';
+}
+
+function MapToList(map, mapper, sorter){
+	var list = Object.keys(map).map(function(key){
+		if(mapper){
+			return mapper(map[key], key);	
+		}
+		else{
+			return map[key];
+		}
+	});
+	if(sorter){
+		list = list.sort(sorter);
+	}
+	return list;
+}
+
+function constructBuilding(bData, sData){
+	var resData = {
+		floors: {}
+	}
+	var directory = {};
+	for(var f in bData.floors){
+		var floor = bData.floors[f]
+		resData.floors[f] = {
+			name: floor.name,
+			rooms: {}
+		}
+	}
+	for(var r in bData.rooms){
+		var room = bData.rooms[r];
+		directory[r] = room.floor;
+		resData.floors[room.floor].rooms[r] = {
+			name: room.name,
+			spots: {}
+		}
+	}
+	for(var s in sData){
+		var sensor = sData[s];
+		resData.floors[directory[sensor.room]].rooms[sensor.room].spots[s] = {
+			open: sensor.open
+		}
+	}
+	resData.floors = MapToList(resData.floors, function(floor){
+		floor.rooms = MapToList(floor.rooms, function(room){
+			room.spots = MapToList(room.spots, function(spot){
+				return spot;
+			});
+			return room;
+		});
+		return floor;
+	})
+	return resData;
 }
 
 function getHTML(data){
@@ -50,10 +109,11 @@ function getHTML(data){
 		for(var r = 0; r < floor.rooms.length; r++){
 			var room = floor.rooms[r];
 			var spotHTML = getTag('h2', room.name);
-			spotHTML += room.spots.reduce(function(a, b){
-				var aH = getTag('spot', a.open ? 'Y' : 'N');
-				var bH = getTag('spot', b.open ? 'Y' : 'N');
-				return aH + bH;
+			room.spots.forEach(function(a){
+				var aH = getTag('spot', a.open ? 'Y' : 'N', {
+					classes: [a.open ? 'open' : 'taken']
+				});
+				spotHTML += aH;
 			});
 			html += getTag('room', spotHTML);
 		}
@@ -61,5 +121,14 @@ function getHTML(data){
 	return html;
 }
 
-var roomHTML = getHTML(ROOM_DATA);
-document.body.innerHTML = roomHTML;
+var db = firebase.database();
+db.ref().on('value', function(snapshot){
+	var val = snapshot.val();
+	console.log(val);
+	var inData = constructBuilding({
+		floors: val.floors,
+		rooms: val.rooms
+	}, val.sensors);
+	var roomHTML = getHTML(inData);
+	document.body.innerHTML = roomHTML;
+});
